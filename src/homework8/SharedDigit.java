24192eddb0bb323fa2031107060dec9d5f3a0ca9
Divya <divyapatel.dp908@gmail.com>
2022-08-04 00:29:33 -0400

    data updated2

--- src/homework8/SharedDigit.java
+++ src/homework8/SharedDigit.java
@@ -1,19 +1,28 @@
 package homework8;
 
-/* public class SharedDigit {
-     public static void main(String[] args) {
+public class SharedDigit {
 
-        System.out.println(hasSharedDigit(12, 23));
+    public static void hasSharedDigit(int fNumber, int sNumber) {
 
-    }
-
-    public static boolean hasSharedDigit(int fNumber, int sNumber) {
-        if(fNumber >=10 && fNumber <=99 &&  sNumber >=10 && sNumber <=99 || fNumber /10 == sNumber /10)
-       // if ((fNumber % 10 == sNumber % 10 || (sNumber % 10 == fNumber))) {
-
-            return true;
+        if (fNumber >= 10 && fNumber <= 99 && sNumber >= 10 && sNumber <= 99) {
+            if ((fNumber / 10) == (sNumber / 10) || (fNumber / 10) == (sNumber % 10) || (fNumber % 10) == (sNumber / 10) || (fNumber % 10) == (sNumber % 10)) {
+                boolean s = true;
+                System.out.println(s);
+            }
+        } else {
+            boolean s = false;
+            System.out.println(s);
         }
-        return false;
     }
-   }
-} */
+    public static void main(String[] args) {
+
+       hasSharedDigit(12, 23);
+       hasSharedDigit(9, 99);
+       hasSharedDigit(15, 55);
+       hasSharedDigit(20, 25);
+
+
+    }
+
+
+}
